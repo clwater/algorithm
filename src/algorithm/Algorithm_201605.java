73ce1a370df1b07e82f17b7db353a3cb8c548890
clwater <gengzhibo@gengzhibo.net>
2016-05-07 21:28:41 +0800

    20160507

--- src/algorithm/Algorithm_201605.java
+++ src/algorithm/Algorithm_201605.java
@@ -23,5 +23,33 @@ public class Algorithm_201605 {
         return -1;
 	}
 	
+	
+	
+	//输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+	public static boolean VerifySquenceOfBST(int[] sequence) {
+        if(sequence==null||sequence.length==0)
+            return false;
+        int root=sequence[sequence.length-1];
+        int i=0;
+        for(;i<sequence.length-1;i++){
+            if(sequence[i]>root){
+                break;
+            }
+        }
+        int j=i;
+        for(;j<sequence.length-1;j++){
+            if(sequence[j]<root)
+                return false;
+        }
+        boolean left=true;
+        boolean right=true;
+        if(i>0){
+            left=VerifySquenceOfBST(Arrays.copyOfRange(sequence, 0, i));
+        }
+        if(i<sequence.length-1)
+            right=VerifySquenceOfBST(Arrays.copyOfRange(sequence, i, sequence.length-1));
+        return (left&&right);
+ 
+    }
     
 }
