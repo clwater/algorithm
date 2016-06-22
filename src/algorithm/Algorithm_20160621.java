7dad457a8a97e91d169518e26fcd4a754941337d
clwater <gengzhibo@gengzhibo.net>
2016-06-22 13:48:16 +0800

    20160622

--- src/algorithm/Algorithm_20160621.java
+++ src/algorithm/Algorithm_20160621.java
@@ -25,7 +25,17 @@ public class Algorithm_20160621 {
 	  * 返回：[[7,4,1],[8,5,2],[9,6,3]]*/
 	
 	 public int[][] transformImage(int[][] mat, int n) {
-		 
+		 int temp;
+		 for(int i=0;i<n/2;i++){
+		     for(int j=i;j<n-1-i;j++){
+		         temp = mat[i][j];
+		         mat[i][j]=mat[n-j-1][i];
+		         mat[n-j-1][i]=mat[n-i-1][n-j-1];
+		         mat[n-i-1][n-j-1]=mat[j][n-i-1];
+		         mat[j][n-i-1]=temp;
+		     }
+		  
+		 }
 	       return mat;
 	    }
 }
